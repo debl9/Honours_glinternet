74b8d59064bce0d271c07c703f60f962cc86db20
debl9 <39320926+debl9@users.noreply.github.com>
2018-10-20 19:29:25 +1100

    additional codes

---
+++ Rcodes/bootstrap_lasso_ridge.R
@@ -0,0 +1,53 @@
+
+# Bootstrap Lasso ---------------------------------------------------------
+
+bootlasso <- function (X, Y, numLevels, B=50, nLambdas=20, maxLambda=2.294837e-02, 
+                            minLambda=6.830241e-05, n = nrow(glmnet.matrix2)) {
+  
+  lambdas <- seq(maxLambda, minLambda, length.out = nLambdas)
+  cl <- parallel::makeCluster(parallel::detectCores())
+  doParallel::registerDoParallel(cl)
+  library(foreach)
+  output <- foreach::foreach(b = 1:B, .packages = "glmnet") %dopar% {
+    bootsamps <- sample(x = 1:n, size = n, replace = TRUE)
+    fit <- glmnet::glmnet(x = X[bootsamps,], y = Y[bootsamps],
+                                  lambda = lambdas, 
+                                  family = "binomial", alpha = 1)
+    notbootsamps <- (1:n)[!(1:n) %in% bootsamps]
+    ypred <- predict(fit, newx = X[notbootsamps,], type = "link")
+    return(list(model = fit, bootsamps = bootsamps, notbootsamps = notbootsamps,
+                ypred = ypred))
+  }
+  save(output, file = "bootstrapped_lasso.rda")
+  parallel::stopCluster(cl)
+}
+
+bootlasso(glmnet.matrix2, Y, nLambdas=20, maxLambda=2.294837e-02, 
+          minLambda=6.830241e-05, n = nrow(glmnet.matrix2))
+
+# Bootstrap Ridge ---------------------------------------------------------
+
+bootridge <- function (X, Y, numLevels, B=50, nLambdas=20, maxLambda=22.948368741, 
+                       minLambda=0.002294837, n = nrow(glmnet.matrix2)) {
+  
+  lambdas <- seq(maxLambda, minLambda, length.out = nLambdas)
+  cl <- parallel::makeCluster(parallel::detectCores())
+  doParallel::registerDoParallel(cl)
+  library(foreach)
+  output <- foreach::foreach(b = 1:B, .packages = "glmnet") %dopar% {
+    bootsamps <- sample(x = 1:n, size = n, replace = TRUE)
+    fit <- glmnet::glmnet(x = X[bootsamps,], y = Y[bootsamps],
+                          lambda = lambdas, 
+                          family = "binomial", alpha = 0)
+    notbootsamps <- (1:n)[!(1:n) %in% bootsamps]
+    ypred <- predict(fit, newx = X[notbootsamps,], type = "link")
+    return(list(model = fit, bootsamps = bootsamps, notbootsamps = notbootsamps,
+                ypred = ypred))
+  }
+  save(output, file = "bootstrapped_ridge.rda")
+  parallel::stopCluster(cl)
+}
+
+bootridge(glmnet.matrix2, Y, nLambdas=20, maxLambda=22.948368741, 
+          minLambda=0.002294837, n = nrow(glmnet.matrix2))
+
---
+++ Rcodes/code8_roc_analysis.R
@@ -0,0 +1,77 @@
+
+# ROC Analysis Codes ------------------------------------------------------
+# This script is used to generate the ROC summary statistics and cost function to 
+# determine the optimal threshold cut-off.
+
+# Loading necessary files and libraries
+
+library(pROC)
+load(file = "bootstrapped_glint.rda")   # glinternet bootstraps
+load(file = "bootstrapped_lasso.rda")   # lasso bootstraps
+load(file = "bootstrapped_ridge.rda")   # ridge bootstraps
+
+boot_slice <- function (o) {
+  ypred <- o$ypred
+  bootsamps <- o$bootsamps
+  notbootsamps <- o$notbootsamps
+  slice <- matrix(nrow=n, ncol=nLambda)
+  slice[notbootsamps,] <- ypred
+  return(slice)
+}
+n <- length(Y)
+nLambda <- ncol(output[[1]]$ypred)
+B <- length(output) # 50 bootstrap samples
+
+# Extracting the predictions from each bootstrap sample (50)
+OOBpred <- lapply(output, FUN = function (o) boot_slice(o))
+
+predvstrue <- function (bs, opred, l) {
+  notbootsamps <- opred[[bs]]$notbootsamps
+  ypred <- (OOBpred[[bs]][,l])[!is.na(OOBpred[[bs]][,l])] # ignore NAs, ypred for each lambda value
+  ypred <- exp(ypred)/(1+exp(ypred)) # Transform to obtain probabilities of default
+  best_df <- data.frame(Y[notbootsamps], ypred)
+  return(best_df) 
+}
+
+# Choose the optimal lambda from our results to be 'l'
+# l=19 (glinternet), l=5 (lasso), l=19 (ridge)
+l=19
+cl <- parallel::makeCluster(parallel::detectCores())
+doParallel::registerDoParallel(cl)
+library(foreach)
+predvstrue_df <- foreach::foreach(bs=1:B, .combine = "rbind") %dopar% {
+  predvstrue(bs, opred = output, l)
+}
+parallel::stopCluster(cl)
+
+
+# AUC Tables --------------------------------------------------------------
+rocval <- pROC::roc(predvstrue_df$Y.notbootsamps., predvstrue_df$ypred)
+roctable1 <- pROC::coords(rocval, x = seq(0.01,0.9,0.01), ret = c("accuracy", "ppv", "npv",
+                                                                  "sensitivity", "specificity"))
+roctable2 <- pROC::coords(rocval, x = seq(0.01,0.1,0.01), ret = c("accuracy", "ppv", "npv",
+                                                               "sensitivity", "specificity"))
+
+pROC::coords(rocval, x = "best", ret = c("accuracy", "ppv", "npv",
+                                                   "sensitivity", "specificity",
+                                                   "threshold"))
+
+# Calculate Optimal Cutoff Threshold --------------------------------------
+# a = cost of a false positive (i.e. non defaulter classified as defaulter)
+# b = cost of a false negative (i.e. defaulter classified as non-defaulter)
+a = 1
+b = 5 # cost of missing a default
+p = sum(predvstrue_df$Y.notbootsamps.)/(length(predvstrue_df$Y.notbootsamps.))
+
+# Calculate the expected loss between false positive/false negative 
+# (1-p)*a*(1-specificity) + p*b*(1-sensitivity)
+
+expected_loss <- (1-p)*a*(1-roctable1[5,]) + p*b*(1-roctable1[4,])
+expected_loss_df <- data.frame(expected_loss, highlight = ifelse(expected_loss == min(expected_loss), T , F))
+
+qplot(seq(0.01, 0.9, 0.01), expected_loss) +
+  xlab("Cutoff Thresholds") +
+  ylab("Expected Loss Values") +
+  geom_point(aes(colour = expected_loss_df$highlight)) + 
+  scale_color_manual(values = c("black", "red")) + 
+  theme(legend.position="none")
